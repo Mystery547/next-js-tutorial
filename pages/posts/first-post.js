2d2d2644281dc66f0d29bd333b9472062162dfb9
Ricardo <ricardofs547@gmail.com>
2021-03-11 12:00:50 -0300

    adding styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,31 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+const YourComponent = ()=> {
+  return (
+    <Image src="/images/profile.jpg" width={144} height={144} alt="Your name"/>
+  )
+}
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+        <YourComponent/>
+      </Layout>
     </>
   )
+
 }
+
